9c11db5aad11ab3f983c06b5d2485a46dfb41e53
jooeyp <jpalazzo_2769@email.ric.edu>
2019-03-26 22:19:29 -0400

    fixed upload sorry

---
+++ HubGrubv2/HubGrubv2/Models/RestaurantModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HubGrubv2.Models
+{
+    public class RestaurantModel : DbContext
+    {
+        [Key]
+        public string RestuarantName { get; set; }
+        public string RestaurantApps { get; set; }
+        public string RestaurantEntrees { get; set; }
+        public string RestaurantDeserts { get; set; }
+        public string RestaurantDrinks { get; set; }
+    }
+}
+
+
