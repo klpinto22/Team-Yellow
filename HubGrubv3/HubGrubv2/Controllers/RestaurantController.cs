65f950e3bcb22aa65b9bdca952f8d7ebd9233a80
jooeyp <jpalazzo_2769@email.ric.edu>
2019-03-31 21:43:15 -0400

    this is the latest version. inside the folder it says 'v2' but it is updated.

---
+++ HubGrubv3/HubGrubv2/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using HubGrubv2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HubGrubv2.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        AppDbContext dbcontext = new AppDbContext();
+
+        public IActionResult Index()
+        {
+            return View(dbcontext.RestaurantModel.ToList());
+        }
+        
+    }
+}
---
+++ HubGrubv3/HubGrubv2/Models/RestaurantModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HubGrubv2.Models
+{
+    public class RestaurantModel : DbContext
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+        public string RestaurantName { get; set; }
+        public string RestaurantHours { get; set; }
+        public string RestaurantAddress { get; set; }
+        public int RestaurantPhoneNumber { get; set; }
+    }
+}
+
+
